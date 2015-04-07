c1d04e5ff6e85f6c140db6476b32e8d9d1b0da84
rockytang <rocky.tang168@gmail.com>
2015-04-06 19:58:17 -0700

    Add GET and POST

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,25 +1,106 @@
 // YOUR CODE HERE:
 
-var message = {
-  'username': 'kaivon',
-  'text': 'testTest',
-  'roomname': '4chan'
-};
+
+var app = {};
+app.server = 'https://api.parse.com/1/classes/chatterbox'
 
 
-$(".post").click(function(){
+
+app.clearMessages = function(){
+  $(document).ready(function(){
+    $('#chats').html('');
+
+  })
+}
+
+app.addMessage = function(message){
+
+      $(document).ready(function(){
+        var $chats = $('#chats');
+        var $newMessage = $('<div class="message"></div>');
+        $newMessage.html("<p>"+ message.username + message.text +  "</p>");
+        $newMessage.appendTo($chats);
+      })
+}
+
+app.init = function(){};
+app.send = function(message){
   $.ajax({
     // always use this url
-    url: 'https://api.parse.com/1/classes/chatterbox',
+    url: app.server,
     type: 'POST',
     data: JSON.stringify(message),
     contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Message sent');
+      app.addMessage(message);
     },
     error: function (data) {
       // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
       console.error('chatterbox: Failed to send message');
     }
   });
+};
+
+
+
+app.fetch = function(){
+var count = 0;
+    $.ajax({
+      // always use this url
+      url: app.server,
+      data: {order: "-updatedAt", limit: 20},
+      type: 'GET',
+      // data: JSON.parse(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log(data);
+
+        $(document).ready(function(){
+            var $messages = $('#newMessages');
+            $messages.html('');
+
+            var index = data.results.length - 1;
+
+            while(index >= 0){
+              var message = data.results[index];
+              var $message = $('<div class="message"></div>');
+
+              $message.html("<p>"+ message.username + ":" + message.text +  "</p>");
+
+              $message.appendTo($messages);
+              index -= 1;
+            }
+        })
+
+        console.log('chatterbox: Message received');
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to receive message');
+      }
+    });
+
+};
+
+$(document).ready(function(){
+  $(".get").on('click',function(){app.fetch();})
+
+  $(".post").on('click', function(){
+    var text = $('.textBox').val();
+    var message = {
+      'username': 'kaivon',
+      'text': text,
+      'roomname': 'supSup'
+    };
+    app.send(message);
+
+  });
 });
+
+
+
+
+
+
+
