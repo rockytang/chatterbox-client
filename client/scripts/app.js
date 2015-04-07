e49363b4e11b1df17931e41829447e944e213353
rockytang <rocky.tang168@gmail.com>
2015-04-07 12:23:58 -0700

    Add app.friends

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,87 +1,5 @@
 // YOUR CODE HERE:
 
-
-var app = {};
-app.server = 'https://api.parse.com/1/classes/chatterbox'
-
-
-
-app.clearMessages = function(){
-  $(document).ready(function(){
-    $('#chats').html('');
-
-  })
-}
-
-app.addMessage = function(message){
-
-      $(document).ready(function(){
-        var $chats = $('#chats');
-        var $newMessage = $('<div class="message"></div>');
-        $newMessage.html("<p>"+ message.username + " : "+ message.text +  "</p>");
-        $newMessage.appendTo($chats);
-      })
-}
-
-app.init = function(){};
-app.send = function(message){
-  $.ajax({
-    // always use this url
-    url: app.server,
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message sent');
-      app.addMessage(message);
-    },
-    error: function (data) {
-      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message');
-    }
-  });
-};
-
-
-
-app.fetch = function(){
-    $.ajax({
-      // always use this url
-      url: app.server,
-      data: {order: "-updatedAt", limit: 20},
-      type: 'GET',
-      // data: JSON.parse(message),
-      contentType: 'application/json',
-      success: function (data) {
-        console.log(data);
-
-        $(document).ready(function(){
-            var $messages = $('#newMessages');
-            $messages.html('');
-
-            var index = data.results.length - 1;
-
-            while(index >= 0){
-              var message = data.results[index];
-              var $message = $('<div class="message"></div>');
-
-              $message.html("<p>"+ message.username + ":" + message.text +  "</p>");
-
-              $message.appendTo($messages);
-              index -= 1;
-            }
-        })
-
-        console.log('chatterbox: Message received');
-      },
-      error: function (data) {
-        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to receive message');
-      }
-    });
-
-};
-
 $(document).ready(function(){
   $(".get").on('click',function(){app.fetch();})
 
@@ -108,21 +26,64 @@ $(document).ready(function(){
       'text': text,
       'roomname': room,
     };
-console.log("get chat room!")
-    app.fetchRoom(room);
+    app.fetchRoom(user, room);
   });
 
+  $("body").on('click', 'a', function(){
+    var person = $(this).data("name");
+    app.friends[person] = person
+
+    console.log($(this).data("name"));
+  })
 });
 
-// , order: "-updatedAt", limit: 20},
+var app = {};
+app.server = 'https://api.parse.com/1/classes/chatterbox'
 
-app.fetchRoom = function(room){
-  console.log(typeof room);
-  console.log( room);
+app.init = function(){};
+
+app.friends = {};
+
+app.clearMessages = function(){
+  $(document).ready(function(){
+    $('#chats').html('');
+
+  })
+}
+
+app.addMessage = function(message){
+
+      $(document).ready(function(){
+        var $chats = $('#chats');
+        var $newMessage = $('<div class="message"></div>');
+        $newMessage.html("<p>"+ message.username + " : "+ message.text + "</p>");
+        $newMessage.appendTo($chats);
+      })
+}
+
+app.send = function(message){
+  $.ajax({
+    // always use this url
+    url: app.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+      app.addMessage(message);
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+app.fetch = function(){
     $.ajax({
       // always use this url
       url: app.server,
-      data: 'where= {"roomname": "supSup"}'
+      data: {order: "-updatedAt", limit: 20},
       type: 'GET',
       // data: JSON.parse(message),
       contentType: 'application/json',
@@ -139,7 +100,19 @@ app.fetchRoom = function(room){
               var message = data.results[index];
               var $message = $('<div class="message"></div>');
 
-              $message.html("<p>"+ message.username + ":" + message.text +  "</p>");
+              // if this username (message.username) is our friend (in our app.friends object)
+              // then give it's message a friends class
+
+              if(app.friends[message.username]){
+                $message.html("<p class= 'friends'>"+ "<a href='#' data-name="+_.escape(message.username)
+                +" class='friends'>" + _.escape(message.username) + "</a>" + ":" + _.escape(message.text)
+                + "(" + message.createdAt + ")" +  "</p>");
+              } else {
+              // if not, do not give it a friends class
+                $message.html("<p>"+ "<a href='#' data-name="+_.escape(message.username)
+                +">" + _.escape(message.username) + "</a>" + ":" + _.escape(message.text)
+                + "(" + message.createdAt + ")" +  "</p>");
+              }
 
               $message.appendTo($messages);
               index -= 1;
@@ -155,8 +128,49 @@ app.fetchRoom = function(room){
     });
 
 };
-//XSS notes 3 ways
-// underscore escape / regex / or in html?
+
+app.fetchRoom = function(user, room){
+
+    $.ajax({
+      // always use this url
+      url: app.server,
+      data: 'where= {"roomname": "'+room+'", "username": "'+user+'"}', order: "-updatedAt",
+      type: 'GET',
+      // data: JSON.parse(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log(data);
+
+        $(document).ready(function(){
+            var $messages = $('#newMessages');
+            $messages.html('');
+
+            var index = data.results.length - 1;
+
+            while(index >= 0){
+              var message = data.results[index];
+              var $message = $('<div class="message"></div>');
+
+              $message.html("<p>"+ "<a href='#' data-name="+_.escape(message.username) +" class='friends'>"
+              + _.escape(message.username) + "</a>" + ":" + _.escape(message.text) + "(" + message.createdAt + ")" +  "</p>");
+
+              $message.appendTo($messages);
+              index -= 1;
+            }
+        })
+
+        console.log('chatterbox: Message received');
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to receive message');
+      }
+    });
+
+};
+
+// XSS notes: 3 ways to do it
+// underscore escape / regex / or in html
 
 
 
